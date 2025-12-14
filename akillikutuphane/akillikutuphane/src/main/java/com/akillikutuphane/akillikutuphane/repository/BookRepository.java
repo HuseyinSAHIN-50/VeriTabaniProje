bec9c7315e0e8e59cf2b73a8f3d0f023d7379c8f
huseyinsahin50 <50huseyinsahin@gmail.com>
2025-12-14 22:13:26 +0300

    sın
    .

--- akillikutuphane/akillikutuphane/src/main/java/com/akillikutuphane/akillikutuphane/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.akillikutuphane.akillikutuphane.repository;
-
-import com.akillikutuphane.akillikutuphane.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
