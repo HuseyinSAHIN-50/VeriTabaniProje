899f57451612ed450bb806b9fa5ebfdb97e4179b
huseyinsahin50 <50huseyinsahin@gmail.com>
2025-12-14 22:15:32 +0300

    Delete akillikutuphane directory

--- akillikutuphane/akillikutuphane/src/main/java/com/akillikutuphane/akillikutuphane/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.akillikutuphane.akillikutuphane.repository;
-
-import com.akillikutuphane.akillikutuphane.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
